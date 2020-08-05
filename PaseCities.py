17a9ad2fda5c4d72679d7f98a8c57a6aaa217691
Djusey <djuntyan@gmail.com>
2020-08-06 02:18:39 +0300

    BS4 reads html from txt
    Got code of frame and saved it to txt file.
    I want to get a table of cities with values.

--- PaseCities.py
+++ PaseCities.py
@@ -1,13 +1,18 @@
 # importing the libraries
 from bs4 import BeautifulSoup
 import requests
-
-url="https://wordstat.yandex.ru/stat/regions_tree?geo=225"
+import io
+#url="file:///C:/Users/Djusey/PycharmProjects/ParsingYandexWordstat/Parsing%20Yandex%20Wordstat/CitiesFrame.html"
 
 # Make a GET request to fetch the raw HTML content
-html_content = requests.get(url).text
-
+#html_content = requests.get(url).text
+filecities = io.open("CitiesFrame.txt", encoding='utf-8')
 # Parse the html content
-soup = BeautifulSoup(html_content, features="html.parser")
+soup = BeautifulSoup(filecities, features="lxml")
 print(soup.prettify()) # print the parsed data of html
 #print(soup.title.text)
+
+# import pandas as pd
+#
+# tables = pd.read_html("CitiesFrame.html")
+# print(tables[0])
