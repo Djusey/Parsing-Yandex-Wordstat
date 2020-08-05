f14ea35144cd6ff8ec48d8445fd7d3c7ca8a5268
djusey <djuntyan@gmail.com>
2020-08-06 01:38:47 +0300

    Got html file with cities

--- PaseCities.py
+++ PaseCities.py
@@ -2,12 +2,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url="https://wordstat.yandex.ru/#!/history?period=weekly&regions=225"
+url="https://wordstat.yandex.ru/stat/regions_tree?geo=225"
 
 # Make a GET request to fetch the raw HTML content
 html_content = requests.get(url).text
 
 # Parse the html content
 soup = BeautifulSoup(html_content, features="html.parser")
-#print(soup.prettify()) # print the parsed data of html
-print(soup.title.text)
+print(soup.prettify()) # print the parsed data of html
+#print(soup.title.text)
